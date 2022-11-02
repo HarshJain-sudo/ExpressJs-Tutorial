ac9418d7c955c4f3914dfb5a2134588bcd6270b8
hj60490 <harshj60490@gmail.com>
2022-11-03 01:11:43 +0530

    ch8 added

--- ch8/app.js
+++ ch8/app.js
@@ -5,6 +5,20 @@ const app = express()
 
 const port = 3000
 
+app.get('/student/all',(req,res)=>{
+    res.send('All Student')
+})
+
+app.get('/student/delete/:id([0-9]{2})',(req,res)=>{
+    console.log(req.params)
+    const {id}=req.params
+    console.log(id)
+    if(id==10){
+       return  res.send('Not Deleted')
+    }
+    res.send(`Student delete ${id}`)
+
+})
 app.listen(port,()=>{
     console.log(`server is running on port http://localhost:${port}`)
 })
